e189bba74a836590aac469ec59585bbed8c86a37
Jacob Lizarraga <jlizarr1@jhu.edu>
2020-03-03 21:40:42 -0500

    "Added marker and some click functionality"
    Co-authored-by: Jacob Lizarraga <jacobl7893@gmail.com>
    Co-authored-by: Shaurya Kumar <kumar.shaurya13@gmail.com>

--- app/seekr/src/components/pages/GoogleMap.jsx
+++ app/seekr/src/components/pages/GoogleMap.jsx
@@ -1,26 +1,52 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 const mapStyles = {
-  width: '50%',
-  height: '50%'
+  width: '75%',
+  height: '75%'
 };
 
 export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
   render() {
     return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-         lat: -1.2884,
-         lng: 36.8233
-        }}
-      />
+      <Map google={this.props.google}
+      zoom={14}
+      style={mapStyles}
+      initialCenter={{
+       lat: 39.33020225,
+       lng: -76.62183913176027
+      }}>
+        
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <div>
+              <h1>{this.state.selectedPlace.name}</h1>
+            </div>
+        </InfoWindow>
+        
+      </Map>
     );
   }
 }
+
+
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyDlUTl5jQFNPLW_z1wA_C0IXYHfwUvt8V8'
