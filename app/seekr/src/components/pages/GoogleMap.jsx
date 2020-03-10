16480f3b3aba8d26da1bbf2265f462b947e9a12d
Jacob Lizarraga <jlizarr1@jhu.edu>
2020-03-09 21:46:55 -0400

    markers can be added to map on click

--- app/seekr/src/components/pages/GoogleMap.jsx
+++ app/seekr/src/components/pages/GoogleMap.jsx
@@ -6,7 +6,66 @@ const mapStyles = {
   height: '75%'
 };
 
-export class MapContainer extends Component {
+class MainMap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: [
+        {
+          title: "The marker`s title will appear as a tooltip.",
+          name: "SOMA",
+          position: { lat: 37.778519, lng: -122.40564 }
+        }
+      ]
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(t, map, coord) {
+    const { latLng } = coord;
+    const lat = latLng.lat();
+    const lng = latLng.lng();
+
+    this.setState(previousState => {
+      return {
+        markers: [
+          ...previousState.markers,
+          {
+            title: "",
+            name: "",
+            position: { lat, lng }
+          }
+        ]
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="text-center">My Maps</h1>
+        <Map
+          google={this.props.google}
+          style={{ width: "80%", margin: "auto" }}
+          className={"map"}
+          zoom={14}
+          onClick={this.onClick}
+        >
+          {this.state.markers.map((marker, index) => (
+            <Marker
+              key={index}
+              title={marker.title}
+              name={marker.name}
+              position={marker.position}
+            />
+          ))}
+        </Map>
+      </div>
+    );
+  }
+}
+
+/* export class MapContainer extends Component {
   state = {
     showingInfoWindow: false,
     activeMarker: {},
@@ -44,10 +103,10 @@ export class MapContainer extends Component {
       </Map>
     );
   }
-}
+} */
 
 
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyDlUTl5jQFNPLW_z1wA_C0IXYHfwUvt8V8'
-})(MapContainer);
+})(MainMap);
