d39b3909673869652762eabec055a6ae6eb16d5f
Andrew Zhang <31313038+yzhan289@users.noreply.github.com>
2020-03-10 18:11:28 -0400

    changed test config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class TestConfig(object):
-    # MONGO_DBNAME = "testdb"
-    MONGO_URI = "mongodb://user1:admin1@ds019028.mlab.com:19028/heroku_gvwmsj7z"
+    MONGO_DBNAME = "mydb"
+    MONGO_URI = "mongodb://localhost:27017/" + MONGO_DBNAME
 
 class ProdConfig(object):
     # MONGO_DBNAME = "mydb"
