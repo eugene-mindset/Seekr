057023660cc8a0ebbfe90528a836b8d5a2e01559
Andrew Zhang <31313038+yzhan289@users.noreply.github.com>
2020-03-10 18:02:46 -0400

    added mongo uri

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 class TestConfig(object):
-    MONGO_DBNAME = "mydb"
-    MONGO_URI = "mongodb://localhost:27017/" + MONGO_DBNAME
+    # MONGO_DBNAME = "testdb"
+    MONGO_URI = "mongodb://user1:admin1@ds019028.mlab.com:19028/heroku_gvwmsj7z"
 
 class ProdConfig(object):
-    MONGO_DBNAME = "testdb"
-    MONGO_URI = "mongodb://localhost:27017/" + MONGO_DBNAME
+    # MONGO_DBNAME = "mydb"
+    MONGO_URI = "mongodb://user1:admin1@ds019028.mlab.com:19028/heroku_gvwmsj7z"
 
 
