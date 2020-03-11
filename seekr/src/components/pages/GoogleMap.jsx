1064e7bc05a720d6b5d596e3af42db1962c52b78
Anderson Adon <andersonadon@gmail.com>
2020-03-11 13:16:18 -0400

    changed default map location

--- seekr/src/components/pages/GoogleMap.jsx
+++ seekr/src/components/pages/GoogleMap.jsx
@@ -14,8 +14,8 @@ export class MapContainer extends Component {
         zoom={14}
         style={mapStyles}
         initialCenter={{
-         lat: -1.2884,
-         lng: 36.8233
+         lat: 39.3299013,
+         lng: -76.6227065
         }}
       />
     );
