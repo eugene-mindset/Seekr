b2c2ffb8e35f4fec1447c805d2798c5321e6c053
Andrew Zhang <31313038+yzhan289@users.noreply.github.com>
2020-04-22 19:50:50 -0400

    finished authcontext component

--- seekr/src/components/helper/AuthContext.jsx
+++ seekr/src/components/helper/AuthContext.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-export default function AuthContext() {
-	return (
-		<div>
-			
-		</div>
+export const RootContext = React.createContext();
+
+export default function AuthContext({children}) {
+	// check cookie for auth
+	// previous status of authentication
+	const prevAuth = document.cookie.match(new RegExp('(^| )auth=([^;]+)')) || false;
+	
+	// previous authentication information
+	const prevAuthBody = document.cookie.match(new RegExp('(^| )authBody=([^;]+)')) || false;
+	
+	const [auth, setAuth] = useState(prevAuth);
+	const [authBody, setAuthBody] = useState(prevAuthBody);
+
+	// whenever auth or authBody is changed, execute useEffect
+	useEffect(
+		// this anonymous function will automatically update the cookies
+		() => {
+			document.cookie = "auth=" + auth + "; authBody=" + authBody + ";path=/";
+		},
+		[auth, authBody]
 	)
-}
+	
+	const defailtContext = {
+		auth,
+		setAuth,
+		authBody,
+		setAuthBody
+	}
+
+	return (
+		<RootContext.Provider value = {defaultContext}>
+			{children}
+		</RootContext.Provider>
+	);
+};
