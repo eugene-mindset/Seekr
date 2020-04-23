1c7b9d6d606eb5c9ccb84e1eb93ce58535c8c10b
Andrew Zhang <31313038+yzhan289@users.noreply.github.com>
2020-04-22 20:40:12 -0400

    removed context from app.js

--- seekr/src/App.js
+++ seekr/src/App.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Navigation from './components/layout/Navigation';
-import About from './components/pages/About';
-import Search from './components/pages/Search';
-import Add from './components/pages/Add';
-import UserInfo from './components/pages/UserInfo';
+import Navigation from "./components/layout/Navigation";
+import About from "./components/pages/About";
+import Search from "./components/pages/Search";
+import Add from "./components/pages/Add";
+import UserInfo from "./components/pages/UserInfo";
 
-import './App.css';
+import "./App.css";
+import { AuthContext, AuthContextProvider } from "./components/helper/AuthContext";
 
-class App extends Component {
-
-  render() {
-    return (
+const App = () => {
+  return (
+    <AuthContextProvider>
       <Router>
         <div className="App">
           <Navigation />
-          <Route exact path="/" component={Search}/>
+          <Route exact path="/" component={Search} />
           <div className="site-container">
             <Route path="/add" component={Add} />
-            <Route path="/about" component={About} />
-            <Route path="/userinfo" component={UserInfo} />
+            <Route path="/about" component={About} /> 
+            <Route path="/userinfo" component={UserInfo}/>           
           </div>
         </div>
       </Router>
-    );
-  }
-}
+    </AuthContextProvider>
+  );
+};
 
 export default App;
