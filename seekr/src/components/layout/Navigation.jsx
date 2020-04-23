51048317177ce8111b41f7ee9ea1e7172add9582
Andrew Zhang <31313038+yzhan289@users.noreply.github.com>
2020-04-22 20:40:50 -0400

    added auth context so nav items are diff depending on logged in/out

--- seekr/src/components/layout/Navigation.jsx
+++ seekr/src/components/layout/Navigation.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import { Nav, Navbar } from 'react-bootstrap';
+import React, { useContext } from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import { AuthContext } from "../helper/AuthContext";
 
-export default class Navigation extends React.Component {
-  render() {
-    return (
-      <div>
-        <Navbar expand="lg" bg="dark" fixed="top">
-          <Navbar.Brand href="/">Seekr: Lost and Found</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Nav.Link href="/add">Add</Nav.Link>
-              <Nav.Link href="/">Search</Nav.Link>
-              <Nav.Link href="/about">About</Nav.Link>
-              <Nav.Link href="/userinfo">User Info</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
-    );
-  }
-}
+const Navigation = () => {
+  const { auth } = useContext(AuthContext)
+  return (
 
+    <div>
+      <Navbar expand="lg" bg="dark" fixed="top">
+        <Navbar.Brand href="/">Seekr: Lost and Found</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="/add">Add</Nav.Link>
+            <Nav.Link href="/">Search</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            {auth ? <Nav.Link href="/userinfo">User Info</Nav.Link> : null}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Navigation;
