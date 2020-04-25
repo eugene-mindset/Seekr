04417ba6df87a3a3e04626e0adb7e217bb7008d4
Shaurya <kumar.shaurya13@gmail.com>
2020-04-25 15:08:36 -0400

    Stuff for andrew

--- seekr/src/components/pages/Add.jsx
+++ seekr/src/components/pages/Add.jsx
@@ -3,47 +3,9 @@ import axios from 'axios';
 import AddItem from '../item/AddItem';
 import { AuthContext } from '../helper/AuthContext';
 
-export default class Add extends Component {
+// export default class Add extends Component {
 
-  addItem = (name, found, desc, location, tags, img, radius, username, email, phone) => {
-    var data = new FormData();
-    data.append('name', name);
-    data.append('desc', desc);
-    data.append('found', found);
-    data.append('latitude', location[0]);
-    data.append('longitude', location[1]);
-    data.append('radius', radius);
-    data.append('tags', tags);
-    data.append('username', username);
-    data.append('email', email);
-    img.forEach(i => {
-      // Append multiple files to request form
-      data.append('image', i);
-    });
-
-    axios({
-      method: 'post',
-      url: '/items',
-      data: data,
-      headers: {'Content-Type': 'multipart/form-data' }
-      });
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <h1>Add Item</h1>
-        <AddItem addItem={this.addItem} />
-      </React.Fragment>
-    );
-  }
-}
-
-// const Add = () => {
-
-//   const { auth, setAuth, authBody, setAuthBody } = useContext(AuthContext)
-
-//   AddItem = (name, found, desc, location, tags, img, radius, username, email, phone) => {
+//   addItem = (name, found, desc, location, tags, img, radius, username, email, phone) => {
 //     var data = new FormData();
 //     data.append('name', name);
 //     data.append('desc', desc);
@@ -52,8 +14,8 @@ export default class Add extends Component {
 //     data.append('longitude', location[1]);
 //     data.append('radius', radius);
 //     data.append('tags', tags);
-//     data.append('username', auth);
-//     data.append('email', authBody);
+//     data.append('username', username);
+//     data.append('email', email);
 //     img.forEach(i => {
 //       // Append multiple files to request form
 //       data.append('image', i);
@@ -67,13 +29,51 @@ export default class Add extends Component {
 //       });
 //   }
 
-
+//   render() {
 //     return (
 //       <React.Fragment>
 //         <h1>Add Item</h1>
-//         <AddItem addItem={addItem} />
+//         <AddItem addItem={this.addItem} />
 //       </React.Fragment>
 //     );
+//   }
+// }
 
-// };
-// export default Add;
+const Add = () => {
+
+  const { auth, setAuth, authBody, setAuthBody } = useContext(AuthContext)
+
+  const addItem = (name, found, desc, location, tags, img, radius, username, email, phone) => {
+    var data = new FormData();
+    data.append('name', name);
+    data.append('desc', desc);
+    data.append('found', found);
+    data.append('latitude', location[0]);
+    data.append('longitude', location[1]);
+    data.append('radius', radius);
+    data.append('tags', tags);
+    data.append('username', auth);
+    data.append('email', authBody);
+    // img.forEach(i => {
+    //   // Append multiple files to request form
+    //   data.append('image', i);
+    // });
+
+    axios({
+      method: 'post',
+      url: '/items',
+      data: data,
+      headers: {'Content-Type': 'multipart/form-data' }
+      });
+  }
+
+
+    return (
+      <React.Fragment>
+        <h1>Add Item</h1>
+        <AddItem addItem={addItem} />
+      </React.Fragment>
+    );
+
+};
+export default Add;
