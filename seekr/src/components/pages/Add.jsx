d1283c30ffa4432feff0ceafa0c7da30d244cee7
Shaurya <kumar.shaurya13@gmail.com>
2020-04-25 01:46:42 -0400

    trying to make Add.jsx be hooks

--- seekr/src/components/pages/Add.jsx
+++ seekr/src/components/pages/Add.jsx
@@ -1,10 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import axios from 'axios';
 import AddItem from '../item/AddItem';
+import { AuthContext } from '../helper/AuthContext';
 
-export default class Add extends Component {
+// export default class Add extends Component {
 
-  addItem = (name, found, desc, location, tags, img, radius, username, email, phone) => {
+//   addItem = (name, found, desc, location, tags, img, radius, username, email, phone) => {
+//     var data = new FormData();
+//     data.append('name', name);
+//     data.append('desc', desc);
+//     data.append('found', found);
+//     data.append('latitude', location[0]);
+//     data.append('longitude', location[1]);
+//     data.append('radius', radius);
+//     data.append('tags', tags);
+//     data.append('username', username);
+//     data.append('email', email);
+//     img.forEach(i => {
+//       // Append multiple files to request form
+//       data.append('image', i);
+//     });
+
+//     axios({
+//       method: 'post',
+//       url: '/items',
+//       data: data,
+//       headers: {'Content-Type': 'multipart/form-data' }
+//       });
+//   }
+
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <h1>Add Item</h1>
+//         <AddItem addItem={this.addItem} />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const Add = () => {
+
+  const { auth, setAuth, authBody, setAuthBody } = useContext(AuthContext)
+
+  AddItem = (name, found, desc, location, tags, img, radius, username, email, phone) => {
     var data = new FormData();
     data.append('name', name);
     data.append('desc', desc);
@@ -13,8 +52,8 @@ export default class Add extends Component {
     data.append('longitude', location[1]);
     data.append('radius', radius);
     data.append('tags', tags);
-    data.append('username', username);
-    data.append('email', email);
+    data.append('username', auth);
+    data.append('email', authBody);
     img.forEach(i => {
       // Append multiple files to request form
       data.append('image', i);
@@ -28,12 +67,13 @@ export default class Add extends Component {
       });
   }
 
-  render() {
+
     return (
       <React.Fragment>
         <h1>Add Item</h1>
-        <AddItem addItem={this.addItem} />
+        <AddItem addItem={addItem} />
       </React.Fragment>
     );
-  }
-}
+
+};
+export default Add;
