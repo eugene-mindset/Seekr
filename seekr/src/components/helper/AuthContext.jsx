38e8d79b2a89efe2f8bd17ac22e87bc002c01064
Shaurya <kumar.shaurya13@gmail.com>
2020-04-25 22:53:56 -0400

    changed AuthContext to look correct

--- seekr/src/components/helper/AuthContext.jsx
+++ seekr/src/components/helper/AuthContext.jsx
@@ -12,12 +12,17 @@ function getCookieValue(a) {
 export const AuthContextProvider = ({children}) => {
 	// check cookie for auth
 	// previous status of authentication
-	const prevAuth = getCookieValue("auth") || null;
+	const prevName = getCookieValue("name") || null;
 	
 	// previous authentication information
-	const prevAuthBody = getCookieValue("authBody") || null;
+	const prevEmail = getCookieValue("email") || null;
+
+	const prevUserID = getCookieValue("userID") || null;
+
+	const prevProfilePic = getCookieValue("profilePic") || null;
+
 	
-	const [auth, setAuth] = useState(prevAuth);
+	const [name, setName] = useState(prevName);
 	/**
 	 * state {
 	 * 	auth: prevAuth
@@ -26,23 +31,31 @@ export const AuthContextProvider = ({children}) => {
 	 * this.setState{ auth: hello } == setAuth(hello)
 	 * 
 	 */
-	const [authBody, setAuthBody] = useState(prevAuthBody);
+	const [email, setEmail] = useState(prevEmail);
+	const [userID, setUserID] = useState(prevUserID);
+	const [profilePic, setProfilePic] = useState(prevProfilePic);
 
 	// whenever auth or authBody is changed, execute useEffect
 	useEffect(
 		// this anonymous function will automatically update the cookies
 		() => {
-			document.cookie = "auth=" + auth + "; path=/";
-			document.cookie = "authBody=" + authBody + "; path=/";
+			document.cookie = "name=" + auth + "; path=/";
+			document.cookie = "email=" + authBody + "; path=/";
+			document.cookie = "userID=" + userID + "; path=/";
+			document.cookie = "profilePic=" + profilePic +"; path=/";
 		},
 		[auth, authBody]
 	)
 	
 	const defaultContext = {
-		auth,
-		setAuth,
-		authBody,
-		setAuthBody
+		name,
+		setName,
+		email,
+		setEmail,
+		userId,
+		setUserID,
+		profilePic,
+		setProfilePic
 	}
 
 	return (
