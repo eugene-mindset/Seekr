8cd28a0596db9244dae61623ee772336f6dfa3c6
Andrew Zhang <31313038+yzhan289@users.noreply.github.com>
2020-04-26 11:33:14 -0400

    added email to the item to show delete button

--- seekr/src/components/item/Items.jsx
+++ seekr/src/components/item/Items.jsx
@@ -1,5 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import Item from './Item';
+import { AuthContext } from "../helper/AuthContext";
+
 import PropTypes from 'prop-types';
 
 // class Items extends React.Component {
@@ -17,8 +19,10 @@ import PropTypes from 'prop-types';
 // }
 
 const Items = ({items, deleteItem}) => {
+  const { email } = useContext(AuthContext);
+
   return items.map((item) => (
-    <Item key={item.id} item={item} deleteItem={deleteItem} />
+    <Item key={item.id} item={item} deleteItem={deleteItem} email={email} />
   ));
 };
 
