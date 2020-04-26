01bcb2a71004d55f6ae2fe0d1684610d738d0833
Shaurya <kumar.shaurya13@gmail.com>
2020-04-26 14:21:16 -0400

    remove items class code

--- seekr/src/components/item/Items.jsx
+++ seekr/src/components/item/Items.jsx
@@ -4,20 +4,6 @@ import { AuthContext } from "../helper/AuthContext";
 
 import PropTypes from 'prop-types';
 
-// class Items extends React.Component {
-//   render() {
-//     return this.props.items.map((item) => (
-//       <Item key={item.id} item={item} deleteItem={this.props.deleteItem}/>
-//     ));
-//   }
-// }
-
-// //PropTypes for expected props
-// Items.propTypes = {
-//   items: PropTypes.array.isRequired,
-//   deleteItem: PropTypes.func.isRequired
-// }
-
 const Items = ({items, deleteItem}) => {
   const { email } = useContext(AuthContext);
 
