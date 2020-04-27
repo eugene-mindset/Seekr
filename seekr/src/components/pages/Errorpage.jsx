6c0c585fbc87cdb63eef74f4f808973494fdd1c8
Andrew Zhang <31313038+yzhan289@users.noreply.github.com>
2020-04-27 16:26:55 -0400

    fixed routing on frontend depending on logged in or out

--- seekr/src/components/pages/Errorpage.jsx
+++ seekr/src/components/pages/Errorpage.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 const Errorpage = () => {
-    return (
-            <div style={{marginTop: "100px"}}>
-                <h1>Did not Login Please Try Again</h1>
-            </div>
-    )
-}
+  return (
+    <div style={{ marginTop: "100px" }}>
+      <h1>Please Login to add an item!</h1>
+    </div>
+  );
+};
 
 export default Errorpage;
