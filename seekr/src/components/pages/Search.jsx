f1b8da710b30cfa89cd773429893de2380f73534
jz1242 <jz1242@gmail.com>
2020-04-27 20:19:38 -0400

    fix preload cards

--- seekr/src/components/pages/Search.jsx
+++ seekr/src/components/pages/Search.jsx
@@ -14,7 +14,7 @@ function getCookieValue(a) {
 
 export default class Search extends Component {
   componentDidMount() {
-    name="temp";
+    name="%20";
     axios.get("/items/timesearch=" + name).then((res) =>
       res.data.map((item) => {
         return this.setState({ items: [...this.state.items, item] });
@@ -84,7 +84,7 @@ export default class Search extends Component {
           searchItem={this.searchItem}
           clearSearch={this.clearSearch}
         />
-        <div style={{marginLeft: "450px", float:"left"}}>
+        <div style={{marginLeft: "350px", float:"left"}}>
           <CardColumns>
             <Items items={this.state.items} deleteItem={this.deleteItem} />
           </CardColumns>
