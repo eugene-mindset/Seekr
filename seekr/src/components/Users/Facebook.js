6ee447e45532142ff94286296a79956806448652
jz1242 <jz1242@gmail.com>
2020-04-28 15:24:02 -0400

    prod mode app id

--- seekr/src/components/Users/Facebook.js
+++ seekr/src/components/Users/Facebook.js
@@ -51,8 +51,8 @@ const Facebook = () => {
   if (userID == "null" || userID == "undefined") {
     faceContent = (
       <FacebookLogin
-        // appId="2484603928503868"
-        appId="232984641269662"
+        appId="2484603928503868" //prod mode app id
+        // appId="232984641269662" dev mode app id
         autoLoad={false}
         fields="name,email,picture"
         onClick={componentClicked}
