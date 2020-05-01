3ee124e5790ab5b354418fb991a1d607b7b813bb
Jacob Lizarraga <jlizarr1@jhu.edu>
2020-05-01 16:13:39 -0400

    finished Add test

---
+++ seekr/src/__tests__/Add.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render, unmountComponentAtNode } from "react-dom";
+import Add from '../components/pages/Add';
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(<Add/>, div);
+});
