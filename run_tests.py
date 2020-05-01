bbe60ccb9a1a94660e7a11c4ebb53b844a5862df
Andrew Zhang <31313038+yzhan289@users.noreply.github.com>
2020-05-01 13:17:57 -0400

    test pipeline

--- run_tests.py
+++ run_tests.py
@@ -5,6 +5,6 @@ import unittest
 from app import create_app
 from app.python_tests.flask_api_test import *
 from app.python_tests.similarity_test import *
-
+from app.python_tests.User_flask_test import *
 #Run the tests
 unittest.main()
