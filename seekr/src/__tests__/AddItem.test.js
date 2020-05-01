9b030954e3f49ca9c01cd26b1be77e383afab15c
Jacob Lizarraga <jlizarr1@jhu.edu>
2020-05-01 14:14:38 -0400

    fixed overcomplications

--- seekr/src/__tests__/AddItem.test.js
+++ seekr/src/__tests__/AddItem.test.js
@@ -20,31 +20,9 @@ afterEach(() => {
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  const state = {
-    name: '',
-    found: false,
-    desc: '',
-    location: [39.3299, -76.6205],
-    img: [],
-    radius: 0,
-    tags: 0,
-    username: "",
-    email: "",
-    phone: "",
-  };
-
-  submitForm = (name, found, desc, location, tags, img, radius, username, email, phone) => {
-    state.name=name
-    state.desc=desc
-    state.found=found
-    state.location=location
-    state.radius=radius
-    state.tags=tags
-    state.img=img
-    state.username=username
-    state.email=email
-    state.phone=phone
+  function submitForm() {
+    alert('test');
   }
 
-  ReactDOM.render(<AddItem submitForm={submitForm}/>, div);
+  ReactDOM.render(<AddItem submitForm={() => submitForm()}/>, div);
 });
